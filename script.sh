6aaa37c862cafa72220fbbfe996b2a8bfdfb54e9
calegret1 <155742510+calegret1@users.noreply.github.com>
2024-01-05 16:44:54 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -169,8 +169,8 @@ if [ $IS_ENTERPRISE = "True" ]; then
 
     echo -e "\n========= Added Enterprise code under $OE_HOME/enterprise/addons ========="
     echo -e "\n============= Installing Enterprise specific libraries ============"
-    sudo -H pip3 install num2words ofxparse dbfread ebaysdk firebase_admin pyOpenSSL
-    sudo npm install -g less-plugin-clean-css
+   #cristian sudo -H pip3 install num2words ofxparse dbfread ebaysdk firebase_admin pyOpenSSL
+   #cristian sudo npm install -g less-plugin-clean-css
 fi
 
 echo -e "\n========= Create custom module directory ============"
