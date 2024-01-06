95214b2db3c0abd4ab497e2d56c0551e11898ad1
calegret1 <155742510+calegret1@users.noreply.github.com>
2024-01-06 12:06:18 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -106,6 +106,12 @@ sudo pip3 install setuptools wheel
 
 #Cristian
 #echo -e "\n=========== Installing nodeJS NPM and rtlcss for LTR support =================="
+sudo apt-get update && sudo apt-get install -y ca-certificates curl gnupg
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt-get update && sudo apt-get install nodejs -y
+
 #sudo curl -sL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 #sudo apt install -y nodejs npm -y
 #sudo npm install -g --upgrade npm
