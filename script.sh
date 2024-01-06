c46888445d14de14483871308603811e9058086b
calegret1 <155742510+calegret1@users.noreply.github.com>
2024-01-06 12:33:37 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -156,7 +156,7 @@ sudo git clone --depth 1 --branch $OE_VERSION https://www.github.com/odoo/odoo $
 sudo pip3 install -r /$OE_HOME_EXT/requirements.txt
 if [ $IS_ENTERPRISE = "True" ]; then
     # Odoo Enterprise install!
-    sudo pip3 install psycopg2-binary pdfminer.six
+    #cristian sudo pip3 install psycopg2-binary pdfminer.six
     echo -e "\n============ Create symlink for node ==============="
     sudo ln -s /usr/bin/nodejs /usr/bin/node
     sudo su $OE_USER -c "mkdir $OE_HOME/enterprise"
